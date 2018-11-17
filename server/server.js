7bee0ee4af2c9dfe56b470b662924f714d57eac0
Ravi Verma <cs.ravi.verma@gmail.com>
2018-11-17 13:19:51 +0530

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,16 +13,28 @@ var io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('New User Connected!');
-
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()
+  });
   socket.on('createMessage', function(message){
     console.log("Create Message ", message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
   });
-
   socket.on('disconnect', () => {
     console.log('Client disconnected')
   });
